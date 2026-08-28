63e45d181502dc808f438845e54673c5eca89a6d
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-08-28 19:31:21 -0400

    remove nth from end

--- 1337Code/Program.cs
+++ 1337Code/Program.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using static _1337Code.Remove_Nth_Node_From_End_of_List.Solution;
 
 
-var solution = new _1337Code.Valid_Parentheses.Solution();
-var result = solution.IsValid("([)]");
+var solution = new _1337Code.Remove_Nth_Node_From_End_of_List.Solution();
+var result = solution.RemoveNthFromEnd(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
 Console.WriteLine(result);
 Console.ReadLine();
---
+++ 1337Code/Remove Nth Node From End of List/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1337Code.Remove_Nth_Node_From_End_of_List
+{
+    public class Solution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode dummy = new ListNode(0, head);
+            int length = GetLength(head);
+            ListNode tmp = dummy;
+            int count = 0;
+            while (tmp != null)
+            {
+                count++;
+                if (count == length - n + 1)
+                {
+                    tmp.next = tmp.next.next;
+                    break;
+                }
+                tmp = tmp.next;
+            }
+            return dummy.next;
+        }
+
+        private int GetLength(ListNode head)
+        {
+            int length = 0;
+            while (head != null)
+            {
+                length++;
+                head = head.next;
+            }
+            return length;
+        }
+    }
+}
