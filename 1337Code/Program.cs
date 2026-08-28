a55341c5baf9127a43f22b5cdc8fe994239825b7
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-08-28 19:15:02 -0400

    valid paratheses

--- 1337Code/Program.cs
+++ 1337Code/Program.cs
@@ -1,1 +1,7 @@
 ﻿
+
+
+var solution = new _1337Code.Valid_Parentheses.Solution();
+var result = solution.IsValid("([)]");
+Console.WriteLine(result);
+Console.ReadLine();
---
+++ 1337Code/Valid Parentheses/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace _1337Code.Valid_Parentheses
+{
+    public class Solution
+    {
+
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            var pairs = new Dictionary<char, char>
+            {
+                [')'] = '(',
+                ['}'] = '{',
+                [']'] = '['
+            };
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (pairs.TryGetValue(c, out char expected))
+                {
+                    if (stack.Count == 0 || stack.Pop() != expected)
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
