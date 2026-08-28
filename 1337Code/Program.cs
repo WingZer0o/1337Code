4914fdb836db30eaa1b6c1d0b1ddc619c8346d92
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-08-27 22:24:14 -0400

    median sorted arrays

--- 1337Code/Program.cs
+++ 1337Code/Program.cs
@@ -1,7 +1,1 @@
-﻿using _1337Code.Add_Two_Numbers;
-
-ListNode l1 = new ListNode(1);
-ListNode l2 = new ListNode(1, new ListNode(9, new ListNode(9)));
-
-ListNode result = Solution.AddTwoNumbers(l1, l2);
-Console.ReadLine();
+﻿
